fc79cdbfafd106672f536d215b6e3b84baf5d13d
Rayrarpa <rafadevvv@gmail.com>
2026-06-26 16:09:36 -0300

    AAAAAAAA

--- Jogos-Unity-2D-main/Assets/Script/CoinController.cs
+++ Jogos-Unity-2D-main/Assets/Script/CoinController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CoinController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject coinParticle;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -12,6 +13,7 @@ public class CoinController : MonoBehaviour
             Pontuation pontuation = other.GetComponent<Pontuation>();
             pontuation.AddPoints();
             Destroy(this.gameObject);
+            Instantiate(coinParticle, this.transform.position, Quaternion.Euler(-90, 0, 0));
         }
     }
 }
